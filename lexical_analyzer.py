407597eb1d1658952331451beb09e27604aacd8f
manylovmax <maximalist000@gmail.com>
2023-07-22 18:15:16 +0300

    added processing by each symbol

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,14 +1,21 @@
+import string
+
 TOKENS_ADD_INDENT = ['program', 'if']
+TOKEN_ALLOWED_SYMBOLS = string.ascii_letters + string.digits
 
 
 class GrammaticalError(Exception):
     error_text = None
+    line_number = None
+    symbol_number = None
 
-    def __init__(self, error_text):
+    def __init__(self, error_text, line_number, symbol_number):
         self.error_text = error_text
+        self.line_number = line_number
+        self.symbol_number = symbol_number
 
     def __str__(self):
-        return f'Грамматическая ошибка: {self.error_text}'
+        return f'{self.line_number}:{self.symbol_number} Грамматическая ошибка: {self.error_text}'
 
 
 class LexicalAnalyzer:
@@ -26,24 +33,30 @@ class LexicalAnalyzer:
             program_name = None
             current_indent = 0
             open_indent_blocks = []
+            line_number = 0
+            symbol_number = 0
 
             for line in lines:
+                line_number += 1
                 if program_closed:
-                    raise GrammaticalError('неверный токен - конец программы уже был объявлен')
+                    raise GrammaticalError('неверный токен - конец программы уже был объявлен', line_number, symbol_number)
                 # очистить строку от комментариев
                 line_without_comments = line.split('!')[0]
+                # обработка строки посимвольно
+                for c in line_without_comments:
+                    pass
                 tokens = line_without_comments.split(' ')
                 # проверка на завершение блока
                 if program_declared and line.startswith('end') and not program_closed:
                     if tokens[1] == 'program':
                         if not program_name == tokens[2]:
-                            raise GrammaticalError('имя программы не верное')
+                            raise GrammaticalError('имя программы не верное', line_number, symbol_number)
                         else:
                             if len(tokens) == 3:
                                 program_closed = True
                                 continue
                             else:
-                                raise GrammaticalError('только одно имя программы должно идти после end program')
+                                raise GrammaticalError('только одно имя программы должно идти после end program', line_number, symbol_number)
 
                     elif tokens[1] == 'if' and open_indent_blocks[-1] == 'if':
                         open_indent_blocks.pop()
@@ -54,8 +67,10 @@ class LexicalAnalyzer:
                     # проверка на отсутп
                     for i in range(current_indent):
                         if line[i] != ' ':
-                            raise GrammaticalError('неверный токен')
+                            raise GrammaticalError('неверный токен', line_number, symbol_number)
+                        symbol_number += 1
                     line = line[current_indent:]
+                    symbol_number = current_indent
 
                 # если вся строка это комментарий - пропустить строку
                 if line.startswith('!'):
@@ -64,7 +79,7 @@ class LexicalAnalyzer:
                 # проверить на объявление модуля
                 if not program_declared:
                     if len(tokens) != 2 or tokens[0] != 'program':
-                        raise GrammaticalError('программа должна начинаться с "program" и имени программы')
+                        raise GrammaticalError('программа должна начинаться с "program" и имени программы', line_number, symbol_number)
                     else:
                         program_declared = True
                         program_name = tokens[1].strip()
