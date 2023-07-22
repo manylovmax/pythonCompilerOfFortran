55488483f69d4f47b0d832d24a185aca0bcb5335
manylovmax <maximalist000@gmail.com>
2023-07-22 17:32:42 +0300

    check end of block; need better check for indent

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,5 +1,6 @@
 TOKENS_ADD_INDENT = ['program', 'if']
 
+
 class GrammaticalError(Exception):
     error_text = None
 
@@ -21,12 +22,38 @@ class LexicalAnalyzer:
             lines = f.readlines()
             # в текущей реализации в файле может быть только один модуль
             program_declared = False
+            program_closed = False
+            program_name = None
             current_indent = 0
+            open_indent_blocks = []
 
             for line in lines:
+                if program_closed:
+                    raise GrammaticalError('неверный токен - конец программы уже был объявлен')
+                # очистка строки от пробелов в начале и конце
+                stripped_line = line.strip()
+                # очистить строку от комментариев
+                line_without_comments = stripped_line.split('!')[0]
+                tokens = line_without_comments.split(' ')
+                # проверка на завершение блока
+                if program_declared and stripped_line.startswith('end') and not program_closed:
+                    if tokens[1] == 'program':
+                        if not program_name == tokens[2]:
+                            raise GrammaticalError('имя программы не верное')
+                        else:
+                            if len(tokens) == 3:
+                                program_closed = True
+                                continue
+                            else:
+                                raise GrammaticalError('только одно имя программы должно идти после end program')
+
+                    elif tokens[1] == 'if' and open_indent_blocks[-1] == 'if':
+                        open_indent_blocks.pop()
+
+                    current_indent -= 2
+
                 if current_indent:
                     # проверка на отсутп
-                    i = 0
                     for i in range(current_indent):
                         if line[i] != ' ':
                             raise GrammaticalError('неверный токен - должен быть отступ')
@@ -36,14 +63,13 @@ class LexicalAnalyzer:
                 if line.startswith('!'):
                     continue
 
-                # очистить строку от комментариев
-                line_without_comments = line.split('!')[0]
-                tokens = line_without_comments.split(' ')
                 # проверить на объявление модуля
                 if not program_declared:
                     if len(tokens) != 2 or tokens[0] != 'program':
                         raise GrammaticalError('программа должна начинаться с "program" и имени программы')
                     else:
                         program_declared = True
+                        program_name = tokens[1]
+                        current_indent = 2
                 else:
                     pass
